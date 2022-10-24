56b372520381e6827dd66cdd93807ab1a94a5b80
matthew198 <matthew@coretas.ai>
2022-10-24 18:29:19 +0200

    solution longest_substring_without_repeating_characters

--- python/longest_substring_without_repeating_characters.py
+++ python/longest_substring_without_repeating_characters.py
@@ -1,3 +1,7 @@
+# 3. Longest Substring Without Repeating Characters - Medium
+
+# Given a string s, find the length of the longest substring without repeating characters.
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         
