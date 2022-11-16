308bc78e00195a1e3a3d500709b18db6173dd093
matthew198 <matthew@coretas.ai>
2022-11-16 13:17:51 +0200

    solution

---
+++ SQL/1757_recyclable_and_low_fat_products.sql
@@ -0,0 +1,15 @@
+-- 1757. Recyclable and Low Fat Products - Easy 
+
+--  product_id is the primary key for this table.low_fats is an ENUM of type ('Y', 'N')
+--  where 'Y' means this product is low fat and 'N' means it is not. recyclable is an ENUM of types ('Y', 'N') where
+-- 'Y' means this product is recyclable and 'N' means it is not.Write an SQL query to find the ids of products that are both low fat
+--  and recyclable.Return the result table in any order.The query result format is in the following example.
+
+-- Write your MySQL query statement below
+SELECT
+  product_id
+FROM
+  Products
+WHERE
+  low_fats = 'Y'
+  and recyclable = 'Y'
