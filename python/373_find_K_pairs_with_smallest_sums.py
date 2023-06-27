4059ada0bcb935c908c83048f89af29f32bb1cec
matthew198 <matthew@coretas.ai>
2023-06-27 10:52:30 +0200

    solution

---
+++ python/373_find_K_pairs_with_smallest_sums.py
@@ -0,0 +1,44 @@
+# 373. Find K Pairs with Smallest Sums - Medium
+
+# You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+
+# Define a pair (u, v) which consists of one element from the first array and one element from the second array.
+
+# Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
+
+# Example 1:
+
+# Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+# Output: [[1,2],[1,4],[1,6]]
+# Explanation: The first 3 pairs are returned from the sequence: [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+
+from heapq import heappush, heappop
+
+class Solution(object):
+    def kSmallestPairs(self, nums1, nums2, k):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        pairs = []
+        if len(nums1) > len(nums2):
+            tmp = self.kSmallestPairs(nums2, nums1, k)
+            for pair in tmp:
+                pairs.append([pair[1], pair[0]])
+            return pairs
+
+        min_heap = []
+        def push(i, j):
+            if i < len(nums1) and j < len(nums2):
+                heappush(min_heap, [nums1[i] + nums2[j], i, j])
+
+        push(0, 0)
+        while min_heap and len(pairs) < k:
+            _, i, j = heappop(min_heap)
+            pairs.append([nums1[i], nums2[j]])
+            push(i, j + 1)
+            if j == 0:
+                push(i + 1, 0)  # at most queue min(n, m) space
+        return pairs
