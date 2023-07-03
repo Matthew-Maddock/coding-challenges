f57ea527533f9982b956458dadf0c823b2ceef71
matthew198 <matthew@coretas.ai>
2023-07-03 09:43:25 +0200

    solution

---
+++ python/859_buddy_strings.py
@@ -0,0 +1,25 @@
+# 859. Buddy Strings - Easy
+
+# Given two strings s and goal, return true if you can swap two letters in s so the result is equal to goal, otherwise, return false.
+
+# Swapping letters is defined as taking two indices i and j (0-indexed) such that i != j and swapping the characters at s[i] and s[j].
+
+# For example, swapping at indices 0 and 2 in "abcd" results in "cbad".
+
+# Example 1:
+
+# Input: s = "ab", goal = "ba"
+# Output: true
+# Explanation: You can swap s[0] = 'a' and s[1] = 'b' to get "ba", which is equal to goal.
+
+
+class Solution:
+    def buddyStrings(self, A: str, B: str) -> bool:
+        if len(A) != len(B):
+            return False
+        if A == B and len(set(A)) < len(A):
+            return True
+
+        diff = [(a, b) for a, b in zip(A, B) if a != b]
+
+        return len(diff) == 2 and diff[0] == diff[1][::-1]
