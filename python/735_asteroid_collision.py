5f1da04540b14f907c0bc7a21541ac7ecb962d24
matthew198 <matthew@coretas.ai>
2023-07-20 10:32:36 +0200

    solution

---
+++ python/735_asteroid_collision.py
@@ -0,0 +1,36 @@
+# 735. Asteroid Collision - Medium
+
+# We are given an array asteroids of integers representing asteroids in a row.
+
+# For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+
+# Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+
+# Example 1:
+
+# Input: asteroids = [5,10,-5]
+# Output: [5,10]
+# Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
+from typing import List
+
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+
+        for a in asteroids:
+            while stack and a < 0 and stack[-1] > 0:
+                diff = a + stack[-1]
+                if diff < 0:
+                    stack.pop()
+
+                elif diff > 0:
+                    a = 0
+
+                else:
+                    a = 0
+                    stack.pop()
+            if a:
+                stack.append(a)
+
+        return stack
