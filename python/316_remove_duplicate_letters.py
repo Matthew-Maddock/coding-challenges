ab1a79830a481e865ac3d12f056164ff8d6b408e
matthew198 <matthew@coretas.ai>
2023-09-26 10:24:35 +0200

    solution

---
+++ python/316_remove_duplicate_letters.py
@@ -0,0 +1,35 @@
+# 316. Remove Duplicate Letters - Medium
+
+# Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is
+# the smallest in lexicographical order
+#  among all possible results.
+
+
+# Example 1:
+
+# Input: s = "bcabc"
+# Output: "abc"
+# Example 2:
+
+# Input: s = "cbacdcbc"
+# Output: "acdb"
+import collections
+
+
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        ans = []
+        count = collections.Counter(s)
+        used = [False] * 26
+
+        for c in s:
+            count[c] -= 1
+            if used[ord(c) - ord("a")]:
+                continue
+            while ans and ans[-1] > c and count[ans[-1]] > 0:
+                used[ord(ans[-1]) - ord("a")] = False
+                ans.pop()
+            ans.append(c)
+            used[ord(ans[-1]) - ord("a")] = True
+
+        return "".join(ans)
