ef6f75a194da2423e61e6e7cef7d2990796604ca
matthew198 <matthew@coretas.ai>
2023-10-03 12:28:28 +0200

    solution

---
+++ python/1512_number_of_good_pairs.py
@@ -0,0 +1,34 @@
+# 1512. Number of Good Pairs - Easy
+
+# Given an array of integers nums, return the number of good pairs.
+
+# A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+# Example 1:
+
+# Input: nums = [1,2,3,1,1,3]
+# Output: 4
+# Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+# Example 2:
+
+# Input: nums = [1,1,1,1]
+# Output: 6
+# Explanation: Each pair in the array are good.
+# Example 3:
+
+# Input: nums = [1,2,3]
+# Output: 0
+from typing import List
+
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        n = len(nums)
+        NumGoodPairs = 0
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] == nums[j]:
+                    NumGoodPairs += 1
+
+        return NumGoodPairs
